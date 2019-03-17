99ddac787bb3e220b3fac3b8b601c08f2fc7ff93
Niemuuu <yorori.too@gmail.com>
2019-03-17 18:48:35 +0900

    Make accounts-items button to a-link

--- pages/index.vue
+++ pages/index.vue
@@ -21,25 +21,29 @@
         <h3 class="title">
           Accounts
         </h3>
-        <div class="field has-addons">
-          <p class="control">
-            <a class="button" href="http://tmkk.hatenablog.com" alt="Blog link">Blog</a>
-          </p>
-          <p class="control">
-            <a class="button" href="https://twitter.com/niemuuu" alt="Twitter link">Twitter</a>
-          </p>
-          <p class="control">
-            <a class="button" href="https://github.com/niemuuu" alt="GitHub link">GitHub</a>
-          </p>
-          <p class="control">
-            <a class="button" href="https://qiita.com/niemuuu" alt="Qiita link">Qiita</a>
-          </p>
-          <p class="control">
-            <a class="button" href="https://www.wantedly.com/users/37956169" alt="Wantedly link">Wantedly</a>
-          </p>
-          <p class="control">
-            <a class="button" href="https://speakerdeck.com/niemuuu" alt="Speaker Deck link">Speaker Deck</a>
-          </p>
+        <div>
+          <label class="label">Blog</label>
+          <a href="http://tmkk.hatenablog.com" alt="Blog link">http://tmkk.hatenablog.com</a>
+        </div>
+        <div>
+          <label class="label">Twitter</label>
+          <a href="http://twitter.com/niemuuu" alt="Twitter link">http://twitter.com/niemuuu</a>
+        </div>
+        <div>
+          <label class="label">GitHub</label>
+          <a href="https://github.com/niemuuu" alt="GitHub link">https://github.com/niemuuu</a>
+        </div>
+        <div>
+          <label class="label">Qiita</label>
+          <a href="https://qiita.com/niemuuu" alt="Qiita link">https://qiita.com/niemuuu</a>
+        </div>
+        <div>
+          <label class="label">Wantedly</label>
+          <a href="https://www.wantedly.com/users/37956169" alt="Blog link">https://www.wantedly.com/users/37956169</a>
+        </div>
+        <div>
+          <label class="label">Speaker Deck</label>
+          <a href="https://speakerdeck.com/niemuuu" alt="Speaker Deck link">https://speakerdeck.com/niemuuu</a>
         </div>
       </section>
 
