a4122654556854dd0d9abc8b141dd5696ba83c38
toxoxi <yorori.too@gmail.com>
2019-06-12 22:43:58 +0900

    update about-me

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -4,11 +4,12 @@
       About me
     </h3>
     <div class="container">
+      <p>本名は石田友樹、新潟県上越市出身のWebエンジニア。</p>
       <p>工業高校の電気科を卒業後、石油天然ガス開発企業のメンテナンス業に従事、プラントの安定稼働を支える。</p>
       <p>次第にエンジニアという職業に憧れを持ち、独学で開発手法を学びつつ、LPIC（現LinuC）を取得。</p>
       <p>2017年11月にWebエンジニアとして転職、様々なシステム・サービスの開発を行う。</p>
       <br>
-      <p>React.js / Redux / Vue.js / Nuxt.js / TypeScript を用いたSPA開発や、<br>Node.js / Golang を用いたバックエンド開発を得意とする。</p>
+      <p>React.js / Redux / Vue.js / Nuxt.js / TypeScript を用いたSPA開発や、<br>Golang / Node.jsを用いたバックエンド開発を得意とする。</p>
     </div>
   </section>
 </template>
